493c7210d23964ded69cbd517076357534123943
Jonathan Kalbfeld <jonathan.kalbfeld@thoughtwave.net>
2016-12-05 22:14:03 -0800

    apt? wtf

---
+++ fresin/metadata.rb
@@ -0,0 +1,10 @@
+name             'fresin'
+maintainer       'Jonathan Kalbfeld'
+maintainer_email 'jonathan.kalbfeld@thoughtwave.com'
+license          'Apache2'
+description      'Installs Resin.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.2.34'
+
+
+supports 'windows'
