6f9491ff56560ef0673ec4f3d3e4ee8b89d6b477
Jonathan Kalbfeld <jonathan.kalbfeld@gmail.com>
2016-12-18 22:26:14 +0000

    We now have a recipe that downloads, customizes, and starts resin.

--- cookbooks/jresin/recipes/default.rb
+++ cookbooks/jresin/recipes/default.rb
@@ -3,21 +3,22 @@ resin_version = "4.0.49"
 resin_file = "c:/resin-#{resin_version}.zip"
 resin_directory = "c:/resin-#{resin_version}"
 
-directory "#{resin_directory}" do
-	 action :create
-end
-
 remote_file resin_file do
   source "http://www.caucho.com/download/resin-#{resin_version}.zip"
   not_if { ::File.exists?(resin_file) }
 end
 
 execute 'unzip resin' do
-	command "unzip resin-#{resin_version}.zip -d #{resin_directory}"
+  command "unzip -o resin-#{resin_version}.zip -d /c/" 
   creates resin_directory
   not_if { ::File.exists?(resin_directory) }
+end
+
+template "#{resin_directory}/conf/resin.properties" do
+	source 'resin.properties'
 end
 
 link resin_alias do
   to resin_directory
 end
+
