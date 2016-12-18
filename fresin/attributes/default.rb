983c851677e837db22e200a708aa9e0191e1733e
Jonathan Kalbfeld <jonathan.kalbfeld@gmail.com>
2016-12-18 20:01:01 +0000

    make this much simpler.  just set it in the recipe for now.

--- fresin/attributes/default.rb
+++
@@ -1,5 +0,0 @@
-default[:java][:jdk_version] = 7
-default[:resin][:user] = Etc.getlogin
-default[:resin][:home] = File.join(Dir.home(node[:resin][:user]), 'resin')
-default[:resin][:base_dir] = File.expand_path("..", node[:resin][:home])
-default[:resin][:version] = '4.0.49'
